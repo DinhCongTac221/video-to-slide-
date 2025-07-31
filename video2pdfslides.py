7cd26a527219b1ac2e2cdca6c449b6f373ece3db
Dinh Cong Tac <dinhcongtac221@gmail.com>
2025-07-31 14:26:39 +0900

    Update video2pdfslides.py

--- video2pdfslides.py
+++ video2pdfslides.py
@@ -95,7 +95,8 @@ def detect_unique_screenshots(video_path, output_folder_screenshot_path):
 
 def initialize_output_folder(video_path):
     '''Clean the output folder if already exists'''
-    output_folder_screenshot_path = f"{OUTPUT_SLIDES_DIR}/{video_path.rsplit('/')[-1].split('.')[0]}"
+    base_filename = os.path.splitext(os.path.basename(video_path))[0]
+    output_folder_screenshot_path = os.path.join(OUTPUT_SLIDES_DIR, base_filename)
 
     if os.path.exists(output_folder_screenshot_path):
         shutil.rmtree(output_folder_screenshot_path)
